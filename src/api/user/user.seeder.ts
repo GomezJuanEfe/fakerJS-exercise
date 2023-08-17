ad03d7b75e21bb1385372244917d903d77487ef5
Juan Gomez <jf.gomezramirez@gmail.com>
2023-08-17 15:53:17 -0500

    faker js exercise done

--- src/api/user/user.seeder.ts
+++ src/api/user/user.seeder.ts
@@ -1,25 +1,31 @@
 import { hashPasswordSync } from "../../auth/utils/bcrypt"
 import { User as UserModel } from "./user.types"
+import { faker } from '@faker-js/faker'
 
 type User = Omit<UserModel, 'createdAt' | 'updatedAt'>
 
-export const userSeeder: User[] = [
-  {
-    id: 'llf829qw000090mu95flje03',
-    email: 'jhondoe@test.com',
-    firstName: 'Jhon',
-    lastName: 'Doe',
+export const userSeeder: User[] = Array.from({ length: 10}).map(() => {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  return ({
+    id: faker.string.uuid(),
+    firstName: firstName,
+    lastName: lastName,
+    email: faker.internet.email({firstName: firstName, lastName: lastName}),
     password: hashPasswordSync('1234'),
     isActive: true,
     avatar: 'https://picsum.photos/200',
-  },
-  {
-    id: 'llf829qw000090mu95flje04',
-    email: 'jaenndoe@test.com',
-    firstName: 'Jaen',
-    lastName: 'Doe',
-    password: hashPasswordSync('1234'),
-    isActive: false,
-    avatar: 'https://picsum.photos/200',
-  },
-]
+  })
+})
+
+// [
+//   {
+//     id: faker.string.uuid(),
+//     firstName: faker.person.firstName(),
+//     lastName: faker.person.lastName(),
+//     email: faker.internet.email(),
+//     password: hashPasswordSync('1234'),
+//     isActive: true,
+//     avatar: 'https://picsum.photos/200',
+//   }
+// ]
--- src/api/userRole/userRole.seeder.ts
+++ src/api/userRole/userRole.seeder.ts
@@ -1,17 +1,14 @@
 import { roleSeeder } from "../roles/role.seeder";
 import { userSeeder } from "../user/user.seeder";
 
-export const userRoleSeeder = [
-  {
-    roleId: roleSeeder[0].id,
-    userId: userSeeder[0].id,
-  },
-  {
-    roleId: roleSeeder[1].id,
-    userId: userSeeder[1].id,
-  },
-  {
-    roleId: roleSeeder[2].id,
-    userId: userSeeder[1].id,
-  },
-]
+export const userRoleSeeder = userSeeder.map( item => ({
+  roleId: roleSeeder[0].id,
+  userId: item.id,
+}))
+
+// [
+//   {
+//     roleId: roleSeeder[0].id,
+//     userId: userSeeder[0].id,
+//   },
+// ]
